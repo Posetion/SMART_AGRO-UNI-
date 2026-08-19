6f41a09aa93c79959781ba357604e60e3b799713
Kaung Myat Tun <kaungmyattun069@gmail.com>
2026-08-19 10:00:24 +0630

    .

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,8 +1,12 @@
+import dns from 'node:dns';
 import mongoose from 'mongoose';
 import { env } from './env.js';
 
 export async function connectDb(uri = env.MONGODB_URI): Promise<typeof mongoose> {
   mongoose.set('strictQuery', true);
+  if (env.NODE_ENV === 'development') {
+    dns.setServers(['1.1.1.1', '8.8.8.8']);
+  }
   await mongoose.connect(uri);
   return mongoose;
 }
